566eedb0f8e24d8d27059b1499ee0bfdcfa3313d
Ханк <hankdjambong@gmail.com>
2020-05-11 17:49:56 +0300

    V3.0 - Перестроен внутреннюю структуру обработки запросов GET с учетом несколько критериев поиска (surname, name, passportSerial email, country)

--- src/main/java/usersMicroService/handlers/HeartbeatServlet.java
+++ src/main/java/usersMicroService/handlers/HeartbeatServlet.java
@@ -3,7 +3,7 @@ package usersMicroService.handlers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import usersMicroService.models.Answer;
-import usersMicroService.utils.Common;
+import usersMicroService.processes.ResponseProcessing;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,17 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class HeartbeatServlet extends HttpServlet {
+public class HeartbeatServlet extends HttpServlet implements ResponseProcessing {
+
     private static Logger logHeartbeatServlet = LoggerFactory.getLogger(HeartbeatServlet.class.getSimpleName());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            resp.setContentType("application/json");
-            resp.setStatus(HttpServletResponse.SC_OK);
-            Answer answer = new Answer("OK", null, null);
-            resp.getWriter().println(Common.getPrettyGson().toJson(answer));
-        }catch (Exception e)
-        {
+            setRespParameters(resp, HttpServletResponse.SC_OK, new Answer("OK", null, null));
+        } catch (Exception e) {
             logHeartbeatServlet.error("Error. " + e.getMessage());
         }
     }
